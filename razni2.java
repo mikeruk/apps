b4669df4be6d078ec99a710984a201f3efbae454
mikeruk <yanchev.k@gmail.com>
2021-09-11 08:45:42 +0200

    razni2.java

---
+++ razni2.java
@@ -0,0 +1,76 @@
+package bookExercises;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class razni2 {
+
+	public static void main(String[] args) {
+//
+//		Scanner scanner = new Scanner(System.in);
+//
+//		String values = scanner.nextLine();
+//
+//		String[] s = values.split("");
+//		List<String> items = Arrays.stream(values.split(" "))
+//				.collect(Collectors.toList());
+//
+//		List<Integer> nums = new ArrayList<>();
+//
+////		for (int i = 0; i < items.size(); i++)
+////		  nums.add(Integer.parseInt(items.get(i)));
+//
+//
+//
+//
+//		for(int k = 0; k <= s.length-1; k++) {
+//			int num = Integer.parseInt(s[k]);
+//			nums.add(num);
+//		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+//		String input = scanner.nextLine();
+//		String type = "";
+//		
+//		
+//		
+//		while(!input.equals("END")) {
+//			
+//			if(input.equals("true") || input.equals("false")) {
+//				type = "boolean";
+//			}
+//			
+//			
+//			if(input.length() == 1) {
+//				
+//				if(input.charAt(0) < 48 &&  input.charAt(0) > 57) {
+//					type = "char";
+//				} else if(input.charAt(0) < 58 &&  input.charAt(0) > 47) {
+//					type = "integer";
+//				}
+//			} else {
+//				
+//				for(int i = 0; i <= input.length()-1; i++) {
+//				if(input.charAt(i) == 46) {
+//					type = "Floating point";
+//				}
+//				
+//			}
+//		
+//			}
+//		}
+	}
+}
+    	
