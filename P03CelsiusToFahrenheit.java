10c2f053ae25196c325538b8ffc68b1cb12923f8
mikeruk <yanchev.k@gmail.com>
2021-09-25 12:22:53 +0200

    P03CelsiusToFahrenheit.java

---
+++ P03CelsiusToFahrenheit.java
@@ -0,0 +1,18 @@
+package FirstStepsInCodingMoreExercises;
+
+import java.util.Scanner;
+
+public class P03CelsiusToFahrenheit {
+
+	public static void main(String[] args) {
+
+
+		Scanner scanner = new Scanner(System.in);
+		double celsius = Double.parseDouble(scanner.nextLine());
+		double fahrenheit = (celsius * 9/5) + 32;
+		System.out.printf("%.2f", fahrenheit);
+		
+		
+	}
+
+}
