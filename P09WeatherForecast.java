edd37f83ba926cab3d0c0f171ec37468816fe5b6
mikeruk <yanchev.k@gmail.com>
2021-10-04 07:54:57 +0200

    P09WeatherForecast.java

---
+++ P09WeatherForecast.java
@@ -0,0 +1,24 @@
+package FirstStepsInCodingMoreExercises;
+
+import java.util.Scanner;
+
+public class P09WeatherForecast {
+
+	public static void main(String[] args) {
+
+
+		Scanner scanner = new Scanner(System.in);
+		
+		String sunny = scanner.nextLine();
+		
+		
+		if(sunny.equals("sunny")) {
+			System.out.println("It's warm outside!");
+		}else{
+			System.out.println("It's cold outside!");
+		}
+		
+	}
+
+}
+
