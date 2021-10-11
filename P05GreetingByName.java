fb3cb9328ccc3d07218643536ab644973a94b419
mikeruk <yanchev.k@gmail.com>
2021-10-11 13:20:32 +0200

    P05GreetingByName.java

---
+++ P05GreetingByName.java
@@ -0,0 +1,19 @@
+package MyPackageDemo;
+
+import java.util.Scanner;
+
+public class P05GreetingByName {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+			
+		String name = scanner.nextLine();
+		System.out.print("Hello, ");
+		System.out.print(name);
+		System.out.print("!");	
+		
+
+	}
+
+}
