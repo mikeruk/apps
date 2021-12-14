fd37bd105a3c5aa2be5a7b7b88c5604709bfe6b7
mikeruk <yanchev.k@gmail.com>
2021-12-14 08:14:04 +0100

    P11FruitShop.java

---
+++ P11FruitShop.java
@@ -0,0 +1,73 @@
+package tema07ConditionalStatementsAdv;
+
+import java.util.Scanner;
+
+public class P11FruitShop {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		String fruit = scanner.nextLine();
+		String day = scanner.nextLine();
+		double quantity = Double.parseDouble(scanner.nextLine());
+		
+		
+		double price = 0;
+		switch(day) {
+			case "Monday":
+			case "Tuesday":
+			case "Wednesday":
+			case "Thursday":
+			case "Friday":
+				if(fruit.equals("banana")) {
+					price = quantity * 2.5;
+				} else if(fruit.equals("apple")) {
+					price = quantity * 1.2;
+				} else if(fruit.equals("orange")) {
+					price = quantity * 0.85;
+				} else if(fruit.equals("grapefruit")) {
+					price = quantity * 1.45;
+				} else if(fruit.equals("kiwi")) {
+					price = quantity * 2.7;
+				} else if(fruit.equals("pineapple")) {
+					price = quantity * 5.5;
+				} else if(fruit.equals("grapes")) {
+					price = quantity * 3.85;
+				} else {
+					System.out.println("error");
+				}
+				break;
+			case "Saturday":
+			case "Sunday":
+				if(fruit.equals("banana")) {
+					price = quantity * 2.7;
+				} else if(fruit.equals("apple")) {
+					price = quantity * 1.25;
+				} else if(fruit.equals("orange")) {
+					price = quantity * 0.90;
+				} else if(fruit.equals("grapefruit")) {
+					price = quantity * 1.60;
+				} else if(fruit.equals("kiwi")) {
+					price = quantity * 3.0;
+				} else if(fruit.equals("pineapple")) {
+					price = quantity * 5.6;
+				} else if(fruit.equals("grapes")) {
+					price = quantity * 4.20;
+				} else {
+					System.out.println("error");
+				}
+				break;
+			default:
+				System.out.println("error");
+				break;
+		}
+		if(price != 0) {
+			System.out.printf("%.2f",price);
+		} else {
+			
+		}
+	}
+
+}
