d6dbda2450215b4739e95c2285ca2610d7f9210a
mikeruk <yanchev.k@gmail.com>
2022-01-10 06:25:53 +0100

    P02NumbersfromNto1.java

---
+++ P02NumbersfromNto1.java
@@ -0,0 +1,19 @@
+package tema09ForLoop;
+
+import java.util.Scanner;
+
+public class P02NumbersfromNto1 {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner scanner = new Scanner(System.in);
+		int n = Integer.parseInt(scanner.nextLine());
+		
+		for(int i = n; i >= 1; i--) {
+			System.out.println(i);
+		}
+
+	}
+
+}
