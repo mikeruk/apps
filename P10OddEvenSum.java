1490076abc43eb541f71081a707b729f6d0c4131
mikeruk <yanchev.k@gmail.com>
2022-01-27 06:29:31 +0100

    P10OddEvenSum.java

---
+++ P10OddEvenSum.java
@@ -0,0 +1,36 @@
+package tema09ForLoop;
+
+import java.util.Scanner;
+
+public class P10OddEvenSum {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int n = Integer.parseInt(scanner.nextLine());
+		
+		int sumEven = 0;
+		int sumOdd = 0;
+		
+		for(int i = 1; i <= n; i++) {
+			
+			int currentNum = Integer.parseInt(scanner.nextLine());
+			
+			if(i % 2 == 0) {
+				sumEven += currentNum;
+			} else {
+				sumOdd += currentNum;
+			}
+		}
+		
+		
+		if(sumEven == sumOdd) {
+			System.out.printf("Yes%nSum = %d",sumEven);
+		} else {
+			System.out.printf("No%nDiff = %d",Math.abs(sumEven - sumOdd));
+		}
+		
+	}
+
+}
