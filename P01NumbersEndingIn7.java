057708e31de7c02da6e32a95a5a97b5f671c6607
mikeruk <yanchev.k@gmail.com>
2022-01-28 06:36:34 +0100

    P01NumbersEndingIn7.java

---
+++ P01NumbersEndingIn7.java
@@ -0,0 +1,16 @@
+package tema10ForLoopUpr;
+
+import java.util.Scanner;
+
+public class P01NumbersEndingIn7 {
+
+	public static void main(String[] args) {
+		
+		
+		for(int i = 7; i <= 997; i+=10) {
+			System.out.println(i);
+		}
+
+	}
+
+}
