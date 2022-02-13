fd116a17dfc0c9e211bcc8dea55b0a91b4e6f8f4
mikeruk <yanchev.k@gmail.com>
2022-02-13 07:19:10 +0100

    P06Salary.java

---
+++ P06Salary.java
@@ -0,0 +1,45 @@
+package tema10ForLoopUpr;
+
+import java.util.Scanner;
+
+public class P06Salary {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner scanner = new Scanner(System.in);
+		
+
+		int n = Integer.parseInt(scanner.nextLine());
+		int salary = Integer.parseInt(scanner.nextLine());
+		
+		
+		for(int i = 1; i <= n; i++) {
+			
+			String site = scanner.nextLine();
+			if(site.equals("Facebook")) {
+				salary -= 150;
+			} else if(site.equals("Instagram")) {
+				salary -= 100;
+			} else if(site.equals("Reddit")) {
+				salary -= 50;
+			}
+			
+			if(salary <= 0) {
+				break;
+			}
+			
+		}
+		
+		if(salary <= 0) {
+			System.out.println("You have lost your salary.");
+			
+		} else {
+			System.out.printf("%d", salary);
+			
+		}
+		
+		
+	}
+
+}
