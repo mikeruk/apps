c6a8356b428d2b59fc1b412bff7de5d5e0c4737b
mikeruk <yanchev.k@gmail.com>
2022-04-16 13:17:55 +0200

    P03SumNumbers.java

---
+++ P03SumNumbers.java
@@ -0,0 +1,27 @@
+package tema11WhileLoop;
+
+import java.util.Scanner;
+
+public class P03SumNumbers {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int num = Integer.parseInt(scanner.nextLine());
+		
+		int sum = 0;
+		
+		
+		while(sum < num) {
+			
+			int n = Integer.parseInt(scanner.nextLine());
+			sum += n;
+			
+		}
+		
+		System.out.println(sum);
+		
+	}
+
+}
