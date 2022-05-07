ceb3e99d94b676adf6e4c5097dc74712e9ec2406
mikeruk <yanchev.k@gmail.com>
2022-05-07 11:58:01 +0200

    P06MaxNumber.java

---
+++ P06MaxNumber.java
@@ -0,0 +1,28 @@
+package tema11WhileLoop;
+
+import java.util.Scanner;
+
+public class P06MaxNumber {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		
+		String input = scanner.nextLine();
+		
+		int maxNum = Integer.MIN_VALUE;
+		
+		while(!input.equals("Stop")) {
+			int num = Integer.parseInt(input);
+			if(num > maxNum) {
+				maxNum = num;
+			}
+			input = scanner.nextLine();
+		}
+		System.out.println(maxNum);
+
+	}
+
+}
