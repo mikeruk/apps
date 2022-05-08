38ed0082c5b69f4a8e7b30e8881e2ae2d3799652
mikeruk <yanchev.k@gmail.com>
2022-05-08 15:41:43 +0200

    P07MinNumber.java

---
+++ P07MinNumber.java
@@ -0,0 +1,27 @@
+package tema11WhileLoop;
+
+import java.util.Scanner;
+
+public class P07MinNumber {
+
+	public static void main(String[] args) {
+		
+Scanner scanner = new Scanner(System.in);
+		
+		
+		String input = scanner.nextLine();
+		
+		int minNum = Integer.MAX_VALUE;
+		
+		while(!input.equals("Stop")) {
+			int num = Integer.parseInt(input);
+			if(num < minNum) {
+				minNum = num;
+			}
+			input = scanner.nextLine();
+		}
+		System.out.println(minNum);
+
+	}
+
+}
