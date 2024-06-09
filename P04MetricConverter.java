9eff73043ac3d9ce00032d4393816801bd50a6df
mikeruk <90135195+mikeruk@users.noreply.github.com>
2024-06-09 08:10:29 +0200

    P04MetricConverter.java

---
+++ P04MetricConverter.java
@@ -0,0 +1,30 @@
+package Tema0601ProverkiUprajnenie;
+
+import java.util.Scanner;
+
+public class P04MetricConverter {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		double number = Double.parseDouble(scanner.nextLine());
+		String inputMetric = scanner.nextLine();
+		String outputMetric = scanner.nextLine();
+		
+		if(inputMetric.equals("mm")){
+			number = number / 1000;
+		} else if(inputMetric.equals("cm")) {
+			number = number / 100;
+		} 
+		
+		if(outputMetric.equals("mm")) {
+			number = number * 1000;
+		} else if(outputMetric.equals("cm")) {
+			number = number * 100;
+		}
+		
+		System.out.printf("%.3f", number);
+	}
+
+}
