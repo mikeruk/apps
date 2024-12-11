ca040e066adf5a8f480ee67f5be080e6de7ae04c
mikeruk <90135195+mikeruk@users.noreply.github.com>
2024-12-11 19:27:16 +0100

    P03FloatingEquality.java

---
+++ P03FloatingEquality.java
@@ -0,0 +1,27 @@
+package tema09DataTypesAndVariablesMoreExcr;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class P03FloatingEquality {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+        double firstFl = Double.parseDouble(scanner.nextLine());
+        double secondFl = Double.parseDouble(scanner.nextLine());
+        double epsilon = 0.000001;
+        
+        if (firstFl < secondFl) {
+            double temp = firstFl;
+            firstFl = secondFl;
+            secondFl = temp;
+        }
+        if (firstFl - secondFl < epsilon) {
+            System.out.println("True");
+        } else  {
+            System.out.println("False");
+        }
+	}
+
+}
