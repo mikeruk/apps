e00f2a7c933cabce94349f446dd2b35ddc8f644b
mikeruk <90135195+mikeruk@users.noreply.github.com>
2025-02-08 11:14:10 +0100

    P02PascalTriangle.java

---
+++ P02PascalTriangle.java
@@ -0,0 +1,26 @@
+package tema12ArraysExerciseMore;
+
+import java.util.Scanner;
+
+public class P02PascalTriangle {
+
+	public static void main(String[] args) {
+		
+		 int r, i, k, number = 1, j;
+	        Scanner scan = new Scanner(System.in);
+
+	        
+	        r = scan.nextInt();
+
+	        for (i = 0; i < r; i++) {
+	            number = 1;
+	            for (j = 0; j <= i; j++) {
+	                System.out.print(number + " ");
+	                number = number * (i - j) / (j + 1);
+	            }
+	            System.out.println();
+	        }
+
+	}
+
+}
