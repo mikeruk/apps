8c784062173559665bea5663046618437f8196f2
mikeruk <90135195+mikeruk@users.noreply.github.com>
2025-11-24 17:46:53 +0100

    P03CountUppercaseWords.java

---
+++ P03CountUppercaseWords.java
@@ -0,0 +1,26 @@
+package tema11FunctionalProgramming;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class P03CountUppercaseWords {
+
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String[] strings = scanner.nextLine().split("\\s+");
+
+        Predicate<String> predicate = str -> Character.isUpperCase(str.charAt(0));
+
+        List<String> upperCaseStrings = Arrays.stream(strings).filter(predicate)
+                .collect(Collectors.toList());
+
+        System.out.println(upperCaseStrings.size());
+
+        System.out.println(String.join(System.lineSeparator(), upperCaseStrings));
+    }
+}
